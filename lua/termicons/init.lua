a1346af04b7ac4185c278bec201897bc2039c3b0
Mark Skelton <mdskelton99@gmail.com>
2023-03-01 16:22:33 -0500

    Add `setup` function

--- lua/termicons/init.lua
+++ lua/termicons/init.lua
@@ -28,4 +28,14 @@ M.get_overrides_by_filename = function()
 	return map_icons(mappings.icons_by_filename)
 end
 
+M.setup = function()
+	require("nvim-web-devicons").setup({
+		override = M.get_overrides(),
+		override_by_extension = M.get_overrides_by_extension(),
+		override_by_filename = M.get_overrides_by_filename(),
+	})
+
+	require("nvim-web-devicons").set_default_icon("큪", "#6d8086")
+end
+
 return M
