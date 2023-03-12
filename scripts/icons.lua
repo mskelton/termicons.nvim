294b22565841cbdaf10ed16e562a882758979dd4
Mark Skelton <mdskelton99@gmail.com>
2023-03-12 14:32:52 -0500

    More source mappings (#3)
    * Remove color overrides
    
    * Add more mappings

--- scripts/icons.lua
+++ scripts/icons.lua
@@ -1,35 +1,7 @@
 local M = {}
 
 M.by_color = {
-	["advpl-include"] = { "advpl", "#00bcd4" },
-	["advpl-prw"] = { "advpl", "#7986cb" },
-	["advpl-ptm"] = { "advpl", "#ef5350" },
-	["advpl-tlpp"] = { "advpl", "#fbc02d" },
-	["dart-generated"] = { "dart", "#455a64" },
-	["godot-assets"] = { "godot", "#81c784" },
-	["javaclass"] = { "java", "#1e88e5" },
-	["mdsvex"] = { "markdown", "#FF5722" },
-	["nest-controller"] = { "nest", "#0288d1" },
-	["nest-decorator"] = { "nest", "#ab47bc" },
-	["nest-filter"] = { "nest", "#ff7043" },
-	["nest-gateway"] = { "nest", "#afb42b" },
-	["nest-guard"] = { "nest", "#43a047" },
-	["nest-middleware"] = { "nest", "#5c6bc0" },
-	["nest-module"] = { "nest", "#e53935" },
-	["nest-pipe"] = { "nest", "#00897b" },
-	["nest-resolver"] = { "nest", "#ec407a" },
-	["nest-service"] = { "nest", "#ffca28" },
-	["objective-c"] = { "c", "#ffab40" },
-	["objective-cpp"] = { "cpp", "#ffab40" },
 	["react-js"] = { "react", "#ffca28" },
-	["react-ts"] = { "react", "#0288d1" },
-	["redux-action"] = { "redux", "#ab47bc" },
-	["redux-reducer"] = { "redux", "#ef5350" },
-	["redux-selector"] = { "redux", "#ff6e40" },
-	["redux-store"] = { "redux", "#8bc34a" },
-	["svgr"] = { "react", "#ffb300" },
-	["test-js"] = { "test", "#ffca28" },
-	["test-ts"] = { "test", "#0288d1" },
 }
 
 return M
