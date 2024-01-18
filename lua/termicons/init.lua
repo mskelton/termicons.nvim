5ccf21b407e6cc0169a3f482a02aa7c189c31148
Mark Skelton <mdskelton99@gmail.com>
2024-01-17 21:08:27 -0600

    Update API

--- lua/termicons/init.lua
+++ lua/termicons/init.lua
@@ -13,22 +13,36 @@ local function map_icons(icon_set)
 	return res
 end
 
-M.get_overrides_by_extension = function()
+local function get_overrides_by_extension()
 	return map_icons(mappings.by_extension)
 end
 
-M.get_overrides_by_filename = function()
+local function get_overrides_by_filename()
 	return map_icons(mappings.by_filename)
 end
 
+--- Setup the plugin
 M.setup = function()
 	require("nvim-web-devicons").setup({
 		strict = true,
-		override_by_extension = M.get_overrides_by_extension(),
-		override_by_filename = M.get_overrides_by_filename(),
+		override_by_extension = get_overrides_by_extension(),
+		override_by_filename = get_overrides_by_filename(),
 	})
 
 	require("nvim-web-devicons").set_default_icon("큪", "#6d8086")
+end
+
+--- Get an icon by it's canonical name
+--- @param name string
+--- @return string
+--- @throws error if the icon is not found
+M.get = function(name)
+	local icon = icons.icons[name]
+	if not icon then
+		error('Icon "' .. name .. '" not found')
+	end
+
+	return icon.icon
 end
 
 return M
